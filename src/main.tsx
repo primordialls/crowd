7ff54dadb1125f6b8faa49873b610f8fcb037bc3
primordialls <41584147+primordialls@users.noreply.github.com>
2025-03-19 18:26:22 -0400

    okay

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { initializeApp } from "firebase/app";
-import { initializeApp } from "firebase/app";
 import './index.css'
 import App from './App.tsx'
 
