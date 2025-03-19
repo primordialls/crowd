f76a91fd9f48ebd92291a04bbd079308e90b0a44
primordialls <41584147+primordialls@users.noreply.github.com>
2025-03-19 15:23:51 -0400

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <img src="src/assets/hag.jpg" alt="React Logo" className="testImage" />
+      <div className='choiceContainer' >
+        <button className = "choice" onClick={() => console.log('React button clicked')}>
+          <h1>This is AI</h1>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button className = "choice" onClick={() => console.log('Vite button clicked')}>
+        <h1>This is not AI</h1>
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
