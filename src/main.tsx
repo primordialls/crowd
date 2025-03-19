9f0b564aa01c9b0731a5db1481faf828bd90e0b0
primordialls <41584147+primordialls@users.noreply.github.com>
2025-03-19 18:17:25 -0400

    yay

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { initializeApp } from "firebase/app";
 import './index.css'
 import App from './App.tsx'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9vrL8ghxZ1w_FscnrilGhPR4FVlFodpc",
+  authDomain: "crowdsourcing-ca728.firebaseapp.com",
+  projectId: "crowdsourcing-ca728",
+  storageBucket: "crowdsourcing-ca728.firebasestorage.app",
+  messagingSenderId: "886436848185",
+  appId: "1:886436848185:web:40cd22af6cd7fcd88a63e8",
+  measurementId: "G-M2JN1V7G44"
+};
+
+export const app = initializeApp(firebaseConfig);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
